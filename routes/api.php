64659759b2f7d04e32cc5ad2174abc5d9208bc11
Brayansmithe <tchoffobrayan0@gmail.com>
2023-08-27 21:42:35 +0100

    creation des routes pour controllers client

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientControllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//********* * les routes pour le model client*******
+
+Route::get('listeClient',[BoutiqueController::class,'index']);
+Route::post('creeClient',[BoutiqueController::class,'store']);
+Route::get('rechercheClient/{id}',[BoutiqueController::class,'show']);
+Route::put('updateClient/{id}',[BoutiqueController::class,'update']);
+Route::delete('suppresionClient/{id}',[BoutiqueController::class,'destroy']);
