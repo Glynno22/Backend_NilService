db51c8ba09e5fa7f5ab09c615dc14ef79f277964
Lauraineange <angelauraine93@gmail.com>
2023-09-06 15:08:48 +0100

    projet final

--- database/migrations/2023_08_18_002758_create_prestataires_table.php
+++ database/migrations/2023_08_18_002758_create_prestataires_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('prestataire', function (Blueprint $table)  {
+        Schema::create('prestataire', function (Blueprint $table)   {
             $table->increments('id');
             $table->string('nom');
             $table->string('email');
